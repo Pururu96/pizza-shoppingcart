9e0ba0fb5252f5c99a0a49dcd520eb1990dc51a6
Pururu <pururumphahlele@gmail.com>
2021-09-02 17:27:36 +0200

    Edits on HTML and JS files.

--- algorithm.js
+++ algorithm.js
@@ -12,42 +12,145 @@ var buyBtnLarge = document.querySelector(".orderBtnLarge")
 
 var smallQtyCounter = document.querySelector(".smallQty")
 var mediumQtyCounter = document.querySelector(".mediumQty")
-var LargeQtyCounter = document.querySelector(".largeQty")
+var largeQtyCounter = document.querySelector(".largeQty")
 
 var checkoutBtn = document.querySelector(".checkout")
 var paymentAmount = document.querySelector(".paymentAmount")
 var payBtnElement = document.querySelector(".payBtn")
 var messageElement = document.querySelector(".message")
+var payAmount = document.querySelector('.payAmount')
 
+var totalSmallPizza = document.querySelector(".totalSmallPizza")
+var totalMediumPizza = document.querySelector(".totalMediumPizza")
+var totalLargePizza = document.querySelector(".totalLargePizza")
+var cartTotal = document.querySelector(".cartTotal")
 
-var smallPizzaQty = 0
-var mediumPizzaQty = 0
-var LargePizzaQty = 0
-var totalCart = 0
+var smallPizzaQty = 0;
+var mediumPizzaQty = 0;
+var largePizzaQty = 0;
+var totalCart = 0;
 
-function clickBtn(event) {
-    if (event.target.className === addBtnSmall) {
-        alert("Tada!!")
-        // smallPizzaQty++;
-        // smallQtyCounter.innerHTML = smallPizzaQty
+function addButtonSmall() {
+    smallPizzaQty++;
+    smallQtyCounter.innerHTML = smallPizzaQty;
+    totalSmallPizza.innerHTML = (smallPizzaQty * 49).toFixed(2);
+    totalCart = smallPizzaQty * 49 + mediumPizzaQty * 89 + largePizzaQty * 129
+    cartTotal.innerHTML = totalCart.toFixed(2);
+    if (totalCart > 0) {
+        checkoutBtn.classList.remove('hidden');
     }
-
-    console.log()
+    // else if (totalCart=0){
+    //     checkoutBtn.classList.add('hidden');
+    //     payAmount.classList.add('hidden');
+    // }
 }
 
-addBtnSmall.addEventListener('click', clickBtn)
-buyBtnSmall.addEventListener('click', clickBtn)
+function subtractButtonS() {
+    smallPizzaQty--;
+    if (smallPizzaQty < 0) {
+        smallPizzaQty = 0;
+    }
+    smallQtyCounter.innerHTML = smallPizzaQty;
+    totalSmallPizza.innerHTML = (smallPizzaQty * 49).toFixed(2);
+    totalCart = smallPizzaQty * 49 + mediumPizzaQty * 89 + largePizzaQty * 129
+    cartTotal.innerHTML = totalCart.toFixed(2);
+}
+addBtnSmall.addEventListener('click', addButtonSmall)
+buyBtnSmall.addEventListener('click', addButtonSmall)
+subBtnSmall.addEventListener('click', subtractButtonS)
 
 
-// payBtnElement.addEventListener('click', function(){
-//     alert("Hello!")
-// })
+function addButtonMedium() {
+    mediumPizzaQty++;
+    mediumQtyCounter.innerHTML = mediumPizzaQty;
+    totalMediumPizza.innerHTML = (mediumPizzaQty * 89).toFixed(2);
+    totalCart = smallPizzaQty * 49 + mediumPizzaQty * 89 + largePizzaQty * 129
+    cartTotal.innerHTML = totalCart.toFixed(2);
+    if (totalCart > 0) {
+        checkoutBtn.classList.remove('hidden');
+    }
+}
+function subtractButtonM() {
+    mediumPizzaQty--;
+    if (mediumPizzaQty < 0) {
+        mediumPizzaQty = 0;  
+    }
+    mediumQtyCounter.innerHTML = mediumPizzaQty;totalMediumPizza.innerHTML = (mediumPizzaQty * 89).toFixed(2)
+    
+    totalCart = smallPizzaQty * 49 + mediumPizzaQty * 89 + largePizzaQty * 129
+    cartTotal.innerHTML = totalCart.toFixed(2);
+}
+addBtnMedium.addEventListener('click', addButtonMedium)
+buyBtnMedium.addEventListener('click', addButtonMedium)
+subBtnMedium.addEventListener('click', subtractButtonM)
 
-// checkoutBtn.addEventListener('click', function(){
-//     alert("Hello!")
-// })
+function addButtonLarge() {
+    largePizzaQty++;
+    largeQtyCounter.innerHTML = largePizzaQty;
+    totalLargePizza.innerHTML = (largePizzaQty * 129).toFixed(2);
+    totalCart = smallPizzaQty * 49 + mediumPizzaQty * 89 + largePizzaQty * 129
+    cartTotal.innerHTML = totalCart.toFixed(2);
+    if (totalCart > 0) {
+        checkoutBtn.classList.remove('hidden');
+    }
+}
+function subtractButtonL() {
+    largePizzaQty--;
+    if (largePizzaQty < 0) {
+        largePizzaQty = 0;
+    }
+    largeQtyCounter.innerHTML = largePizzaQty;
+    totalLargePizza.innerHTML = (largePizzaQty * 129).toFixed(2)
+    totalCart = smallPizzaQty * 49 + mediumPizzaQty * 89 + largePizzaQty * 129
+    cartTotal.innerHTML = totalCart.toFixed(2);
+}
+addBtnLarge.addEventListener('click', addButtonLarge)
+buyBtnLarge.addEventListener('click', addButtonLarge)
+subBtnLarge.addEventListener('click', subtractButtonL)
 
-// buyBtnLarge.addEventListener('click', function(){
-//     alert("Hello!")
-// })
+function checkoutButton(){
+    checkoutBtn.classList.add('hidden')
+    payAmount.classList.remove('hidden')
+}
+checkoutBtn.addEventListener('click', checkoutButton)
 
+function payment() {
+    messageElement.classList.toggle('hidden')
+    var paymentAmount = Number(payAmount.value)
+    if(paymentAmount == cartTotal) {
+        messageElement.innerHTML = "Enjoy your Pizza!"
+        
+        smallPizzaQty = 0;
+        medPizzaQty = 0;
+        largePizzaQty = 0;
+        totalCart = 0;
+
+        smallQtyCounter.innerHTML = smallPizzaQty;
+        mediumQtyCounter.innerHTML = mediumPizzaQty;
+        largeQtyCounter.innerHTML = largePizzaQty;
+        
+        totalSmallPizza.innerHTML = (smallPizzaQty * 49).toFixed(2);
+        totalMediumPizza.innerHTML = (mediumPizzaQty * 89).toFixed(2);
+        totalLargePizza.innerHTML = (largePizzaQty * 129).toFixed(2);
+        totalCart = smallPizzaQty * 49 + mediumPizzaQty * 89 + largePizzaQty * 129
+        cartTotal.innerHTML = totalCart.toFixed(2);
+
+        setTimeout(function(){
+            messageElement.classList.add('hidden')
+            checkoutBtn.classList.add('hidden')
+            payAmount.classList.add('hidden')
+            payAmount.value = '';
+        }, 4000)
+        // var cartItem = document.getElementsByClassName("payAmount hidden")[0]
+        // while (cartItem.hasChildNodes()) {
+        // cartItem.removeChild(cartItem.firstChild)   
+    }
+    else if (paymentAmount>totalCart){
+        var change = paymentAmount - totalCart
+        messageElement.innerHTML = "Enjoy your Pizza, here is your change R" + change.toFixed(2)
+    }
+    else {
+        messageElement.innerHTML = "Sorry, that is not enough money!"
+    }
+}
+payBtnElement.addEventListener('click', payment)
